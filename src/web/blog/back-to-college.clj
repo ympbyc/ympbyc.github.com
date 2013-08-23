22c4774751f6a5e8fbc9932c16b53549a3c7f47a
Minori Yamashita <ympbyc@gmail.com>
2013-08-22 22:20:47 -0700

    !

--- src/web/blog/back-to-college.clj
+++ src/web/blog/back-to-college.clj
@@ -8,4 +8,4 @@
   [:div
    [:p "This is a lifelog kind of post."]
    [:p " I quit my job at PILOT early this month and am now in the US attending a community college; namely Sierra College at Rocklin, CA. I'm living in the dorms of the campus and I already made some good friends there. I'm sure I'll enjoy this stay."]
-   [:p "Wish me luck."]]})
+   [:p "Wish me luck!"]]})

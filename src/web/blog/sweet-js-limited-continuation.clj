0935db03dc2f0b862d66ae979b35dbebe5679973
Minori Yamashita <ympbyc@gmail.com>
2014-01-11 03:14:19 +0900

    fix typos

--- src/web/blog/sweet-js-limited-continuation.clj
+++ src/web/blog/sweet-js-limited-continuation.clj
@@ -11,17 +11,17 @@
    [:div
     [:h3 "Pyramid of Doom"]
     [:p " JavaScript programs make heavy use of asynchronous functions. Asynchronous functions  in JavaScript are achieved by practicing what's called Continuation Passing Style (CPS) where asynchronous functions are applied to callback functions.  Traditionally, these programs suffered from deep nestings of callback functions due to the lack of syntactic support.  Those nests of callback functions are sometimes called " [:strong "Pyramid of Doom"] " or " [:strong "Callback Hell"] "."]
-    [:p " To solve the issue, programmers started to use constructs known as promises or features. Using promises, JavaScript codes became Limited Continuation Passing Style.  In which nesting don't occur, but the number of callbacks stays the same as before."]
+    [:p " To solve the issue, programmers started to use constructs known as promises or futures. Using promises, JavaScript codes became Limited Continuation Passing Style.  In which nesting don't occur, but the number of callbacks stays the same as before."]
     [:p " In this post, I present a better solution.  I created macros using sweet.js that implements " [:span.code "shift"] " and " [:span.code "reset"] ", two operators that is used to capture limited continuations."]]
    [:div
     [:h3 "Spec"]
-    [:p "Sweet.js provides some ways to define postfix macros.  However, It is impossible to capture continuations of expressions such as " [:span.code "reset(f(g(shift(k=>...))))"] " with a postfix macro.  Due to this restriction, " [:span.code "shift"] " shall be implemented as a statement construct.  This way, the macro only has to look at the statements that follow the "    [:span.code "shift"] " statement."]
+    [:p "Sweet.js provides some ways to define postfix macros.  However, It is impossible to capture continuations of expressions such as " [:span.code "reset(f(g(shift(k=&gt;...))))"] " with a postfix macro.  Due to this restriction, " [:span.code "shift"] " shall be implemented as a statement construct.  This way, the macro only has to look at the statements that follow the "    [:span.code "shift"] " statement."]
     [:pre.prettyprint
      [:code
 "reset {
   ...
-  shift_let <var> = <function>
-  <continuation>...
+  shift_let &lt;var&gt; = &lt;function&gt;
+  &lt;continuation&gt;...
 }"]]
     [:p [:span.code "shift_let"] " takes <function> and give it the continuation. When the continuation gets invoked with a value, the value gets bound to <var>."]]
    [:div
@@ -29,13 +29,13 @@
     [:pre.prettyprint
      [:code
 "macro reset {
-  rule { {$exprs...} } => {
+  rule { {$exprs...} } =&gt; {
     (function () { $exprs... })()
   }
 }
 
 macro shift_let {
-  rule { $var = $fn $rest...} => {
+  rule { $var = $fn $rest...} =&gt; {
     $fn(function ($var) { $rest... })
   }
 }"]]]
